3004224839a39dbed8f3cacdeb5c51f7538f2523
segFault15 <69612295+segFault15@users.noreply.github.com>
2020-09-22 22:07:25 +1000

    First upload

---
+++ example.py
@@ -0,0 +1,9 @@
+from pyerror import Measurement
+
+weight1 = Measurement(15, 0.1)
+weight2 = Measurement(20, 0.5)
+
+print(weight2 + weight1)
+print(weight2 - weight1)
+print(weight2 * weight1)
+print(weight2 / weight1)
