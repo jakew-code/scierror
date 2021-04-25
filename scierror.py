ce71edb9f11432f3e8eb369929046e0c0f131f53
segFault15 <jakewilson198@gmail.com>
2021-04-25 10:52:49 +1000

    Added linear regression

--- scierror.py
+++ scierror.py
@@ -1,4 +1,100 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+class DimensionError(Exception):
+    """An error represeting a mismatch of array dimensions."""
+    def __init__(self, error_message):
+        super().__init__(error_message)
+
+class LinearRegression:
+    """A class to perform and plot linear regressions of data with error in 
+    the dependent variable."""
+    def __init__(self, xdata, ydata, yerr=None):
+        self._xdata = xdata
+        self._ydata = ydata
+        self._yerr = yerr
+
+        self._m = Measurement(0,0)
+        self._c = Measurement(0,0)
+
+    def regress(self):
+        """Caculates the linear fit and returns the gradient and intercept as 
+        measurement objects."""
+        n = len(self._xdata)
+
+        # Check that the length of the arrays are the same.
+        if len(self._ydata) != n:
+            raise DimensionError("dimension mismatch")
+        
+        if self._yerr is None:
+            self._yerr = np.ones(n)*(1/n)
+        elif len(self._yerr) != n:
+            raise DimensionError("dimension mismatch")
+
+        # Check that all of the arrays are numpy arrays.
+        # TODO: Try find a better way to do this.
+        if type(self._xdata) is list:
+            self._xdata = np.array(self._xdata)
+        if type(self._ydata) is list:
+            self._ydata = np.array(self._ydata)
+        if type(self._yerr) is list:
+            self._yerr = np.array(self._yerr)
+
+        # Intermediate calculations.
+        wdata = 1/np.power(self._yerr, 2)
+        xbar = sum(wdata*self._xdata)/sum(wdata)
+        ybar = sum(wdata*self._ydata)/sum(wdata)
+        D = sum(wdata*np.power(self._xdata - xbar, 2))
+
+        # Results.
+        self._m.set_value(1/D * sum(wdata*(self._xdata - xbar)*self._ydata))
+        self._c.set_value(ybar - self._m.get_value()*xbar)
+        arrayD = self._ydata - self._m.get_value()*self._xdata \
+                    - self._c.get_value()
+        self._m.set_error(np.sqrt(1/D * sum(wdata*np.power(arrayD, 2))/(n-2)))
+        self._c.set_error(np.sqrt((1/sum(wdata) + np.power(xbar, 2)/D) 
+                                    * sum(wdata*np.power(arrayD, 2))/(n-2)))
+
+        return self._m, self._c
+
+    def plot(self, title, xlabel, ylabel, filename=None, show=True, useTex=False, xlim=None, ylim=None):
+        """Plots the data points with error bars and the calculated fit.
+        
+        Args:
+            title: Plot title.
+            xlabel: x axis title.
+            ylabel: y axis title.
+            filename: If specified saves plot as filename.
+            show: Set to False to hide plots.
+            useTex: Set to True to render using tex.
+            xlim: Array with x axis bounds.
+            ylim: Array with y axis bounds.
+        """
+        if useTex:
+            plt.rcParams.update({
+                "text.usetex":True,
+                "font.family":"serif"
+            })
+        
+        plt.errorbar(self._xdata, self._ydata, self._yerr, fmt='o', ms=3)
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+
+        # Generate and plot calculated fit.
+        X = np.linspace(self._xdata[0], self._xdata[-1], 100)
+        Y = self._m.get_value()*X + self._c.get_value()
+        plt.plot(X, Y)
+
+        if show:
+            plt.show()
+        
+        if filename is not None:
+            plt.savefig(filename)
+
 
 class Measurement:
     """A class to represent a measurement and the associated 
@@ -15,6 +111,14 @@ class Measurement:
     def get_error(self):
         """Returns the error of the measurement."""
         return self._error
+
+    def set_value(self, value):
+        """Sets the value of the measurement."""
+        self._value = value
+
+    def set_error(self, value):
+        """Sets the error of the measurement."""
+        self._error = value
 
     @staticmethod
     def add_error(error1, error2):
@@ -122,6 +226,6 @@ class Measurement:
         new_error = measurement.get_error() / (measurement.get_value() ** 2 + 1)
         return Measurement(new_value, new_error)
 
-    def __str__(self):
+    def __repr__(self):
         """Returns the string representation of the measurement."""
         return "{} +- {}".format(round(self._value, 4), round(self._error, 4))
